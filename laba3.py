e515ae97f266fc8974c458c01bf891424fc26fb8
KseniaZar55 <160110507+KseniaZar55@users.noreply.github.com>
2024-03-11 19:31:50 +0300

    Delete laba3.py

--- laba3.py
+++
@@ -1,8 +0,0 @@
-words = []
-while True:
-    word = input("Введите слово (или 'стоп' для завершения): ")
-    if word.lower() == 'стоп':
-        break
-    words.append(word)
-result = ' '.join(words)
-print("Результат:", result)
