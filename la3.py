11a893774c5eae813b57d452f2494aaf47afd645
KseniaZar55 <160110507+KseniaZar55@users.noreply.github.com>
2024-03-11 19:31:21 +0300

    Delete la3.py

--- la3.py
+++
@@ -1,7 +0,0 @@
-N = int(input("Введите количество слов: "))
-words = []
-for i in range(N):
-    word = input("Введите слово: ")
-    words.append(word)
-result = ' '.join(words)
-print("Результат:", result)
